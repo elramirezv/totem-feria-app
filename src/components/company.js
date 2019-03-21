ddad00a0831cb3be6385f54361533889ef288e02
Alejandro  Ramirez Vasquez <alekhan123@MacBook-Pro-de-Alejandro.local>
2019-03-21 13:21:11 -0300

    Company component

---
+++ src/components/company.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CompanyComponent extends Component {
+
+  render() {
+    
+    return (
+      <div>
+      <button >{this.props.company.name}</button>
+      </div>
+    );
+  }
+}
+
+export default CompanyComponent;

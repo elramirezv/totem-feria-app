9315d128bdeba241463fedd7811627d0c5e0ee31
Alejandro  Ramirez Vasquez <alekhan123@MacBook-Pro-de-Alejandro.local>
2019-03-21 13:20:22 -0300

    Adding some routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react';
 import CategoriesContainer from './containers/categories';
+import CompaniesContainer from './containers/companies';
 import './assets/css/index.css';
 import data from "./data.json"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
+
   Category({ match }){
+    var companies = {};
     data.forEach((category) => {
-      if(category.name === match.params.name)
-      console.log(category)
-      //return <CompaniesContainer data= category.companies/>
+      if(category.name === match.params.name){
+        companies = category;
+    }
     })
+
+    return <CompaniesContainer data={companies}/>
   }
+  
   Categories(){
     return <CategoriesContainer data={data}/>
   }
 
   render() {
     return (
-      <div className="container-fluid">
-
-      <Router>
       <div>
-      <Route exact path = "/" component = {this.Categories}/>
-      <Route exact path = "/categories" component = {this.Categories}/>
-      <Route path = "/categories/:name" component = {this.Category}/>
-      </div>
+      <Router>
+        <div>
+          <Route exact path = "/" component = {this.Categories}/>
+          <Route path = "/:name" component = {this.Category}/>
+        </div>
       </Router>
     </div>
     );

87d8127b65940b8f403332f9ec3c3ac6b6be12ec
behrmann97 <sbehrmann@uc.cl>
2019-03-25 11:41:13 -0300

    add image

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,6 +6,7 @@ class ProfileComponent extends Component {
     return (
       <div>
       <h1>{this.props.data.name}</h1>
+      <img src={require('../assets/images/' + this.props.data.image)}/>
       </div>
     );
   }

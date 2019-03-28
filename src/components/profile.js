e99830a33f0131c56dbb4ff2b4db410c659fe8e1
behrmann97 <sbehrmann@uc.cl>
2019-03-28 10:23:30 -0300

    video q se ve bacan xd

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import '../styles/profile.css';
+
 
 class ProfileComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        watching: false,
-    }
-    this.moreInfo = this.moreInfo.bind(this)
+        show: false,
+    };
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
 }
-  moreInfo(){
-    this.setState({
-      watching: true
-    })
-  }
+handleClose() {
+  this.setState({ show: false });
+}
+
+handleShow() {
+  this.setState({ show: true });
+}
   render() {
-    if(!this.state.watching){
     return (
-      <div>
+      <>
       <h1>{this.props.data.name}</h1>
-      <img src={require('../assets/images/' + this.props.data.image)}/>
-      <button onClick={this.moreInfo}>más información</button>
-      </div>
+      <img className = 'map-image' src={require('../assets/images/' + this.props.data.image)}/>
+        <Button variant="primary" onClick={this.handleShow}>
+          Ver Video
+        </Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose} dialogClassName="custom-dialog">
+          <Modal.Body>
+
+            <video className = 'video-player' autoPlay loop>
+            <source src={require('../assets/videos/' + this.props.data.video)}/>
+            </video>
+          </Modal.Body>
+        </Modal>
+      </>
     );
-  }
-  else{
-    return(
-      <div>
-      <embed src={require("../assets/pdfs/test.pdf")} type="application/pdf" width="100%" height="600px" />
-      <video width="1000" height = "400" autoPlay loop>
-      <source src={require('../assets/videos/' + this.props.data.video)}/>
-      </video>
-      </div>
-    );
-  }
+    // return(
+    //   <div>
+    //   </div>
+    // );
   }
 }
 

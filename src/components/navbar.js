982c1ab158662ca686c4c31ed1f8f13b5a6459c3
Alejandro  Ramirez Vasquez <alekhan123@MacBook-Pro-de-Alejandro.local>
2019-04-02 14:12:49 -0300

    Navbar and searchbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import Image from 'react-bootstrap/Image';
+import '../assets/css/navbar.css';
+import Navbar from 'react-bootstrap/Navbar';
+
+class NavbarComponent extends Component {
+
+  render() {
+    return (
+      <Navbar>
+      <Navbar.Brand className="navbar-brand-centered" href="/">
+      <Image height={'50px'} width={'180px'} src={require('../assets/logos/logo-filce.png')} rounded />
+      </Navbar.Brand>
+      </Navbar>
+    );
+  }
+}
+
+export default NavbarComponent;
